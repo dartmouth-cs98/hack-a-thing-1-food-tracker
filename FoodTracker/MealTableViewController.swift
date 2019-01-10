899d333e386f234d66bbab7b8b8405f101a4712b
Lindsey Hodel <lindsey.c.hodel.19@dartmouth.edu>
2019-01-10 04:17:42 -0500

    done

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -46,7 +46,6 @@ class MealTableViewController: UITableViewController {
         return meals.count
     }
     
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Table view cells are reused and should be dequeued using a cell identifier.
@@ -66,15 +65,11 @@ class MealTableViewController: UITableViewController {
         return cell
     }
     
-    
-    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    
-    
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -87,8 +82,6 @@ class MealTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
-    
-
     
     //MARK: - Navigation
     
